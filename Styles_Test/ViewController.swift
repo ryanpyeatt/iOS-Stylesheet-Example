77f2b8edda33e1760d352b1b0e75bb3e4a24fe3c
Ryan Pyeatt <ryan.pyeatt@evanta.com>
2018-10-31 11:30:12 -0700

    Initial Commit

---
+++ Styles_Test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Styles_Test
+//
+//  Created by Pyeatt, Ryan on 10/31/18.
+//  Copyright © 2018 Evanta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
